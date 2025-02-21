5d867d45f6a2143fd67dfe988e0e8e8ee17ddc8a
Teja <tejapoosala@gmail.com>
2025-02-21 15:27:13 +0530

    files

--- App/src/main/java/com/example/ci_cdsample/MainActivity.kt
+++ App/src/main/java/com/example/ci_cdsample/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.ci_cdsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        val mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        mainBinding.button.setOnClickListener {
+            "Hello ${mainBinding.editTextText.text}".also { mainBinding.textView.text = it }
         }
     }
 }
